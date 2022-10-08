1302f052101d263ce69c9f841a63191f0b27bdba
Barat-Tojiyev <barattojiyev@gmail.com>
2022-10-08 14:19:29 +0500

    fef

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
+import {data} from './Counter/data'
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      mode:data,
+      search:'',
+      salom:true     
+    }
+  }
   render() {
+
+
     return (
-      <div>App</div>
+      <div>
+         <button onClick={()=>this.setState({salom:!this.state.salom})}>Register</button>
+         <button onClick={()=>this.setState({salom:!this.state.salom})}>Login</button>
+        {
+          this.state.salom ? (
+<div>
+          
+        <input type="text" placeholder='register'  />
+        <input type="text" placeholder='register'  />
+        <input type="text" placeholder='register'  />
+
+        </div>
+          ):(
+<div>
+         
+        <input type="text" placeholder='login'  />
+        
+
+        </div>
+          )
+        }
+        
+        
+
+        
+        
+
+      </div>
     )
   }
 }
---
+++ src/Counter/data.js
@@ -0,0 +1,10 @@
+import { ReactComponent as minus } from './image/sos.svg'
+import { ReactComponent as plus } from './image/sos.svg'
+
+export const data =[
+    {id:1,name:'olma', icon:minus, color:'red'},
+    {id:2,name:'anor', icon:plus, color:'red'},
+    {id:3,name:'banan', icon:minus, color:'red'},
+    {id:4,name:'limon', icon:plus, color:'red'},
+
+]
