8c011080bc2d253e200599ab3c0b7ccc111176df
Barat-Tojiyev <barattojiyev@gmail.com>
2022-10-09 14:30:35 +0500

    dg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
-
 export default class App extends Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state={
+   user:true 
+    }
+  }
+  render() {      
     return (
-      <div>App</div>
+      <div>
+        {
+          this.state.user ?(
+     <div>
+        <button onClick={()=>this.setState({user:!this.state.user})}>Login</button>
+        <input type="text" placeholder='Login' />
+        <input type="password" placeholder='Password' />        
+      </div> ):
+          (<div>
+            <button onClick={()=>this.setState({user:!this.state.user})}>Register</button>
+            <input type="text" placeholder='LastName'/>
+            <input type="text" placeholder='Surname'/>
+            <input type="number" placeholder='Age'/>          
+          </div>       
+          )
+        }    
+     
+      </div>
     )
   }
 }
---
+++ src/Counter/data.js
@@ -0,0 +1,7 @@
+ import  Gul  from "./image.svg"
+export  const data=[
+    {id:1, img:Gul, name:'olma'},
+    {id:2, img:Gul, name:'banan'},
+    {id:3, img:Gul, name:'limon'},
+    {id:4, img:Gul, name:'gilos'},
+]
